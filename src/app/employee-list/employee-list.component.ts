7f48cb212e1563395dacba11ea881ac57669efbc
Gabriel Moraes <gmoraes@ie.com.br>
2017-07-10 17:42:03 -0300

    Creating the employees-list component

---
+++ src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'employee-list',
+    template: `<h1>TESTE LISTA DE EMPREGADOS!!!</h1>`
+})
+export class EmployeeListComponent { 
+}
