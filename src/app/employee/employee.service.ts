a4e4335389da4ee4aff9ee1ecb4e668152a861ab
Gabriel Moraes <gmoraes@ie.com.br>
2017-07-18 17:52:41 -0300

    Creates edit, update and creates functions

--- src/app/employee/employee.service.ts
+++ src/app/employee/employee.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import {Http, Response }         from '@angular/http';
-import {Observable} from 'rxjs/Rx';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 
 import { Employee } from './employee';
@@ -11,6 +11,17 @@ export class EmployeeService {
 
     constructor (private http: Http) {}
 
+    createEmployee(employee): any {
+      let headers = new Headers({ 'Content-Type': 'application/json' });
+      let options = new RequestOptions({ headers: headers });
+
+      return this.http.post(this.employeesUrl, employee);
+    }
+
+    updateEmployee(employee): any {
+      return this.http.put(this.employeesUrl, employee);
+    }
+
     getEmployees(): Observable<Employee[]> {
         return this.http.get(this.employeesUrl)
                         .map((res:Response) => res.json().data);
@@ -18,6 +29,11 @@ export class EmployeeService {
 
     getEmployee(id: number): Observable<Employee> {
         return this.http.get(this.employeesUrl + '/' + id)
+                        .map((res:Response) => res.json());
+    }
+
+    deleteEmployee(id: number): any {
+        return this.http.delete(this.employeesUrl + '/' + id)
                         .map((res:Response) => res.json());
     }
 }
